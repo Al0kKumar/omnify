158fbbdfbef718e5a38e30d059e4f8db442fdddb
Alok <mishraalok189381@gmail.com>
2025-09-03 19:42:53 +0530

    server setup

---
+++ omnify/src/main/java/com/assignment/omnify/OmnifyApplication.java
@@ -0,0 +1,15 @@
+package com.assignment.omnify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableMongoAuditing
+public class OmnifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnifyApplication.class, args);
+	}
+
+}
---
+++ omnify/src/test/java/com/assignment/omnify/OmnifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.omnify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
