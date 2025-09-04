8a29c87b7c01efe9c507574fa606d1762c0a0d05
Alok <mishraalok189381@gmail.com>
2025-09-04 16:10:34 +0530

    added app properties example file

--- backend/src/main/java/com/assignment/omnify/repository/BlogRepository.java
+++ backend/src/main/java/com/assignment/omnify/repository/BlogRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface BlogRepository extends MongoRepository<Blog, String> {
-   List<Blog> findByUserId(String userId);
+   List<Blog> fikendByUserId(String userId);
 }
