677e7cb877ee1d1b8b93e051c5deb5e2e7acbe56
Alok <mishraalok189381@gmail.com>
2025-09-05 09:35:12 +0530

    addded logs

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_API_URL; 
 
+console.log("backend url is " , API_BASE);
+
 export const api = axios.create({
   baseURL: API_BASE,
   headers: {
